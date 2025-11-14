463ab4b881a70da875eda18219fc6e74da104892
cornelius <cornelius.lejeune@googlemail.com>
2025-11-14 15:20:19 +0100

    Init commit

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+import eslintPluginPrettierRecommended  from 'eslint-plugin-prettier/recommended'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react/prop-types': 'off',
+      'react/no-unescaped-entities': 'off',
+
+      'prettier/prettier': [
+        'error',
+        {
+          singleQuote: true,
+          semi: false,
+        },
+      ],
+
+    },
+  },
+  eslintPluginPrettierRecommended,
+])
